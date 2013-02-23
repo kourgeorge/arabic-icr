1af526c66b04de4c9977f1719678efc5b57eb6a4
kourgeorge@gmail.com <kourgeorge@gmail.com@23b4f04e-482a-c3a5-d353-cb0f12a588d0>
2013-02-23 13:36:54 +0000

    Add handling for reverse points. reduce the max slope.
    git-svn-id: https://arabic-icr.googlecode.com/svn/trunk@51 23b4f04e-482a-c3a5-d353-cb0f12a588d0

--- Code/CalculateSlope.m
+++ Code/CalculateSlope.m
@@ -5,7 +5,13 @@ function [ Slope ] = CalculateSlope(Sequence,Point1,Point2)
 start_env= Sequence(Point1,:);
 end_env= Sequence(Point2,:);    
 Slope = abs((end_env(2)-start_env(2))/-(end_env(1)-start_env(1))); %Arabic is written right to left
-if (Slope>1000)
-    Slope = 1000;
+if (end_env(1)>start_env(1))
+    Slope = -1/Slope;
 end
-   
+
+if (Slope>100)
+    Slope = 100;
+end
+if (Slope<-100)
+    Slope = -100;
+end   
