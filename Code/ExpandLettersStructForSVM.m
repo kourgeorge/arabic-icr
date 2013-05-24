303453accd43a72eb25802921cff6e1c0f83f2a5
kourgeorge@gmail.com <kourgeorge@gmail.com@23b4f04e-482a-c3a5-d353-cb0f12a588d0>
2013-05-24 19:45:48 +0000

    performance enhancement to ExpandLettersStructForSVM.m alter the function to return both the samples in the wavelet and the feature domain.
    git-svn-id: https://arabic-icr.googlecode.com/svn/trunk@116 23b4f04e-482a-c3a5-d353-cb0f12a588d0

--- Code/ExpandLettersStructForSVM.m
+++ Code/ExpandLettersStructForSVM.m
@@ -1,20 +1,19 @@
-function [ FeaturesArray, LettersArray , NumericLabeling] = ExpandLettersStructForSVM( PositionLettersDS )
+function [ FeaturesArray, WaveletsArray , LettersArray , NumericLabeling] = ExpandLettersStructForSVM( PositionLettersDS )
 %TESTSVM Summary of this function goes here
 %   Detailed explanation goes here
 
 FeaturesArray = [];
+WaveletsArray = [];
 LettersArray = [];
 NumericLabeling = [];
 
 for i = 1:size(PositionLettersDS,1)
     Letter = PositionLettersDS{i,1};
-    %PositionLettersDS{i,2}; for feature vectores and
-    %PositionLettersDS{i,3}; for wavelets
-    LetterCandidates = PositionLettersDS{i,3};
-    LetterSamples = cell2mat(LetterCandidates)';
-    FeaturesArray = [FeaturesArray; LetterSamples];
-    LettersArray = [LettersArray; repmat(Letter,size(LetterSamples,1),1)];  
-    NumericLabeling = [NumericLabeling; repmat(double(Letter),size(LetterSamples,1),1)];
+    letterSamplesWavelets = cell2mat(PositionLettersDS{i,3})';
+    WaveletsArray = [WaveletsArray; letterSamplesWavelets];
+    FeaturesArray = [FeaturesArray; PositionLettersDS{i,2}'];
+    LettersArray = [LettersArray; repmat(Letter,size(letterSamplesWavelets,1),1)];  
+    NumericLabeling = [NumericLabeling; repmat(double(Letter),size(letterSamplesWavelets,1),1)];
 
 end
 
