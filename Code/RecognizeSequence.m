76943175989395b0355738efcaa24297ce57268a
kourgeorge@gmail.com <kourgeorge@gmail.com@23b4f04e-482a-c3a5-d353-cb0f12a588d0>
2013-05-24 07:16:14 +0000

    Support multi stroke word parts.
    git-svn-id: https://arabic-icr.googlecode.com/svn/trunk@112 23b4f04e-482a-c3a5-d353-cb0f12a588d0

--- Code/RecognizeSequence.m
+++ Code/RecognizeSequence.m
@@ -6,15 +6,28 @@ function [RecognitionResults, SumDist] = RecognizeSequence (Sequence , RecParams
 
 C = [];
 
-% if (strcmp(Position,'Fin') || strcmp(Position,'Mid'))
-%     C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Mid', Alg)];
-%     C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Fin', Alg)];
-% else
-%     C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Iso', Alg)];
-%     C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Ini', Alg)];
-% end
+if (strcmp(Position,'Mid'))
+    C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Mid', RecParams)];
+end
+if (strcmp(Position,'Fin'))
+    C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Fin', RecParams)];
+    C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Mid', RecParams)];
+end
+if (strcmp(Position,'Iso'))
+    C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Iso', RecParams)];
+    C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Ini', RecParams)];
+    C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Fin', RecParams)];
+    C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Mid', RecParams)];
+end
 
-C = [C; RecognizeLetter( Sequence, LettersDataStructure, Position, RecParams)];
+if (strcmp(Position,'Ini'))
+    C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Ini', RecParams)];
+    C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Mid', RecParams)];
+    C = [C; RecognizeLetter( Sequence, LettersDataStructure, 'Fin', RecParams)];
+    
+end
+
+%C = [C; RecognizeLetter( Sequence, LettersDataStructure, Position, RecParams)];
 
 RecognitionResults = sortrows(C,2);
 RecognitionResults = RecognitionResults(1:min(RecParams.NumCandidates,end),:);
