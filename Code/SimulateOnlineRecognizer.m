13de6c001f78befe3c3eaf290f51b0ea39a9586c
kourgeorge@gmail.com <kourgeorge@gmail.com@23b4f04e-482a-c3a5-d353-cb0f12a588d0>
2013-05-28 21:26:44 +0000

    adapting SimulateOnlineRecognizer.m to be able to recognize words instead of word parts. major change.
    git-svn-id: https://arabic-icr.googlecode.com/svn/trunk@137 23b4f04e-482a-c3a5-d353-cb0f12a588d0

--- Code/SimulateOnlineRecognizer.m
+++ Code/SimulateOnlineRecognizer.m
@@ -1,6 +1,6 @@
 function RecState = SimulateOnlineRecognizer( sequence, loadDataStructure, showUI )
 %SIMULATEONLINERECOGNIZER This funtion simulate the Online pen recognizer.
-% a = dlmread(['C:\OCRData\GeneratedWords\<>.m']);
+% a = dlmread(['C:\OCRData\WPsLabeled\.m']);
 % Res = SimulateOnlineRecognizer(a,true,true)
 
 
@@ -16,11 +16,6 @@ RecState = InitializeRecState();
 
 sequence = NormalizeCont(sequence);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%% Sequence Pre-Processing %%%%%%%%%%%%%%%%%%
-% NormalizedLetterSequence = NormalizeCont(sequence);
-% SimplifiedLetterSequence = SimplifyContour( NormalizedLetterSequence);
-% sequence = ResampleContour(SimplifiedLetterSequence,300);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%  Enable Gui  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if (nargin<3)
@@ -33,7 +28,13 @@ if (UI == true)
     himage = figure;
     h_axes = axes();
     set(h_axes,'Tag','AXES');
-    maxX = max(sequence(:,1)); minX = min(sequence(:,1)); maxY = max(sequence(:,2)); minY = min(sequence(:,2));
+    Contour = sequence;
+    temp = Contour(:,1);
+    Contourtemp(:,1) = temp(temp~=Inf('single'));
+    temp=  Contour(:,2);
+    Contourtemp(:,2) = temp(temp~=Inf('single'));
+    
+    maxX = max(Contourtemp(:,1)); minX = min(Contourtemp(:,1)); maxY = max(Contourtemp(:,2)); minY = min(Contourtemp(:,2));
     windowSize = max(maxX-minX,maxY-minY);
     ylim([minY-0.1*windowSize minY+windowSize+0.1*windowSize]);
     xlim([minX-0.1*windowSize minX+windowSize+0.1*windowSize]);
@@ -41,23 +42,35 @@ if (UI == true)
     hold(h_axes,'on');
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
+%Get the indexes of [fin Fin] rows (which symbols Pen UP)
 len = size(sequence,1);
-Sequence = [];
-for k=1:len-1
-    Sequence=[Sequence;sequence(k,:)];
-    RecState = ProcessNewPoint(RecParams,RecState,Sequence,false,UI);
+InfIndexes = find (sequence(:,1) == Inf);
+InfIndexes = [0;InfIndexes;len+1];
+
+for j=1:length(InfIndexes)-1
+    strokes{j} = sequence(InfIndexes(j)+1:InfIndexes(j+1)-1,:);
+end
+
+for j=1:size(strokes,2)
+    RecState = InitializeRecState();
+    stroke = strokes{j};
+    %sequence = NormalizeCont(sequence);
+    strokeLen = size(stroke,1);
+    Stroke = [];
+    for k=1:strokeLen-1
+        Stroke = [Stroke;stroke(k,:)];
+        RecState = ProcessNewPoint(RecParams,RecState,Stroke,false,UI);
+        if (UI == true)
+            plot(h_axes,[stroke(k,1) stroke(k+1,1)],[stroke(k,2) stroke(k+1,2)],'b.-','Tag','SHAPE','LineWidth',3);
+        end
+    end
+    RecState = ProcessNewPoint(RecParams,RecState,stroke,true,UI);
+    
     if (UI == true)
-        plot(h_axes,[sequence(k,1) sequence(k+1,1)],[sequence(k,2) sequence(k+1,2)],'b.-','Tag','SHAPE','LineWidth',3);
+        GetCandidatesFromRecState( RecState )
+        %close (himage);
     end
 end
-RecState = ProcessNewPoint(RecParams,RecState,sequence,true,UI);
-if (UI == true)
-    GetCandidatesFromRecState( RecState )
-    close (himage);
-end
-
 %%%%%%%%%%%%%%%%    Initialization Functions   %%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function RecState = InitializeRecState()
@@ -74,9 +87,9 @@ RecState.LastSeenHorizontalPoint = -1;
 function RecParams = InitializeRecParams()
 % Algorithm parameters
 RecParams.Alg = {'EMD'}; %Res_DTW
-RecParams.K = 5;
-RecParams.PointEnvLength = 5;
+RecParams.K = 3;
+RecParams.PointEnvLength =1;
 RecParams.MaxSlopeRate = 0.6;
 RecParams.MaxDistFromBaseline = 0.15;
-RecParams.NumCandidates = 1;
+RecParams.NumCandidates = 5;
 
