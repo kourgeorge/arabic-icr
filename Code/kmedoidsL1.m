847211f53019ed1a21f2fa4f135fbc914085f13e
kourgeorge@gmail.com <kourgeorge@gmail.com@23b4f04e-482a-c3a5-d353-cb0f12a588d0>
2013-06-01 14:15:42 +0000

    calc kmedoids, with given replication rate and choose the best clustering.
    git-svn-id: https://arabic-icr.googlecode.com/svn/trunk@147 23b4f04e-482a-c3a5-d353-cb0f12a588d0

--- Code/kmedoidsL1.m
+++ Code/kmedoidsL1.m
@@ -1,7 +1,21 @@
-function [label, energy, index] = kmedoidsL1(X,k)
+function [label, energy, index] = kmedoidsL1(X,k,Replications)
 % X: d x n data matrix
 % k: number of cluster
 
+energy = [];
+for i=1:Replications
+    [label_i, energy_i, index_i] = kmedoids_inner(X,k);
+    res(i).label = label_i;
+    energy(i) = energy_i; 
+    res(i).index = index_i;
+end
+[~,ind] = min (energy);
+
+label = res(ind).label;
+index = res(ind).index;
+
+
+function [label, energy, index] = kmedoids_inner(X,k)
 D = mandist(X); 
 n = size(X,2);
 [~, label] = min(D(randsample(n,k),:));
