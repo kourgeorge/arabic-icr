11325e57892e980016a37c00fcc74a0f6b239aca
kourgeorge@gmail.com <kourgeorge@gmail.com@23b4f04e-482a-c3a5-d353-cb0f12a588d0>
2013-06-14 05:43:33 +0000

    move files.
    git-svn-id: https://arabic-icr.googlecode.com/svn/trunk@162 23b4f04e-482a-c3a5-d353-cb0f12a588d0

--- Code/Utils/ADAB Words Generator/ArabicAscii.m
+++
@@ -1,5 +0,0 @@
-function [theLetter] = ArabicAscii(filename,UPXtarget)
-% This function to get the the WORD name from a file
-strfortext=char(strcat(UPXtarget,filename,'.upx'));
-xmlToMatlabStruct = theStruct(strfortext);
-theLetter=xmlToMatlabStruct.Children(1,6).Children(1,2).Children(1,2).Children(1,2).Attributes.Value;
