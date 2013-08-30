5de3c5e3304da3df99f87e7297ffd958b8cba1a0
kourgeorge@gmail.com <kourgeorge@gmail.com@23b4f04e-482a-c3a5-d353-cb0f12a588d0>
2013-08-30 07:42:19 +0000

    Add the slope to the complexity calculation.
    git-svn-id: https://arabic-icr.googlecode.com/svn/trunk@225 23b4f04e-482a-c3a5-d353-cb0f12a588d0

--- Code/InformationMeasure.m
+++ Code/InformationMeasure.m
@@ -1,23 +1,27 @@
-function [ InfoMeas ] = InformationMeasure( Sequence, epsilon )
+function [ InfoMeas ] = InformationMeasure( Sequence, epsilon, maxSlope )
 %INFORMATIONMEASURE This function calculates the entropy of a 2-d signal
 %   Detailed explanation goes here
 
 InfoMeas = 0;
-if (size(Sequence,1)<3)
-    return
+% If the seuqnce contains only a single point
+if (size(Sequence,1)<2)
+    return;
 end
 
-[abs] = SimplifyContour(Sequence, epsilon);
+[absSimplifiedSequence] = SimplifyContour(Sequence, epsilon);
 
-if (size(abs,1)<3)
-    return
-end
-for i=2:(length(abs)-1)
-   v1 = abs(i-1,:)-abs(i,:);
-   v2 = abs(i+1,:)-abs(i,:);
+% if (size(absSimplifiedSequence,1)<3)
+%     return
+% end
+for i=2:(length(absSimplifiedSequence)-1)
+   v1 = absSimplifiedSequence(i-1,:)-absSimplifiedSequence(i,:);
+   v2 = absSimplifiedSequence(i+1,:)-absSimplifiedSequence(i,:);
    theta = acos(dot(v1,v2)/(norm(v1)*norm(v2)));
    InfoMeas = InfoMeas + (pi-theta)/(pi/6);
 end
-    
+
+Slope = CalculateSlope(Sequence,1,length(Sequence));
+InfoMeas = InfoMeas + abs(Slope/maxSlope);
+
 end
 
